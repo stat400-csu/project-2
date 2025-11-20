41240824cc97b335dd918574311cd8b684a05a08
Chase Bishop <cgbishop@colostate.edu>
2025-11-20 11:22:34 -0700

    Update EloStart.R

--- EloStart.R
+++ EloStart.R
@@ -2,3 +2,11 @@ library(tidyverse)
 
 file <- "2024_standings_2025_odds(in)"
 standings_odds <- read_csv(file)
+
+weights <- c(0.4, 0.6)
+mean_elo <- 1000
+scale <- 50
+
+start_elo <- standings_odds |>
+  mutate(Weighted_ProjW = weights[1] * W + weights[2] * ProjW) |>
+  mutate(Start_Elo = (Weighted_ProjW - 8.5) * scale + mean_elo)
