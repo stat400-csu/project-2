381e7b0189e8fb537207208fc54402cf0369c68f
noahsturgeon <sturgeonnoah2@gmail.com>
2025-11-20 22:24:50 -0700

    Arranged the teams in start_elo alphabetically.

--- EloStart.R
+++ EloStart.R
@@ -8,5 +8,6 @@ mean_elo <- 1000
 scale <- 50
 
 start_elo <- standings_odds |>
+  arrange(Tm) |>
   mutate(Weighted_ProjW = weights[1] * W + weights[2] * ProjW) |>
   mutate(Start_Elo = (Weighted_ProjW - 8.5) * scale + mean_elo)
