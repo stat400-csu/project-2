fd6acf3b12949cefbd1f22acdc44fcbe4d603cd0
noahsturgeon <sturgeonnoah2@gmail.com>
2025-11-21 21:39:21 -0700

    Added HFA to elo_start

--- EloStart.R
+++ EloStart.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 
-file <- "2024_standings_2025_odds(in).csv"
-standings_odds <- read_csv(file)
+standings_odds <- read_csv("2024_standings_2025_odds(in).csv")
+
+hfa_data <- read_csv("NFL-HomeFieldAdvantage-Data.csv")
 
 weights <- c(0.4, 0.6)
 mean_elo <- 1000
@@ -9,5 +10,7 @@ scale <- 50
 
 start_elo <- standings_odds |>
   arrange(Tm) |>
+  rename(Team_Name = Tm) |>
   mutate(Weighted_ProjW = weights[1] * W + weights[2] * ProjW) |>
-  mutate(Start_Elo = (Weighted_ProjW - 8.5) * scale + mean_elo)
+  mutate(Elo = (Weighted_ProjW - 8.5) * scale + mean_elo) |>
+  mutate(HFA = hfa_data$Total)
--- WinProbabilityCalculator.R
+++ WinProbabilityCalculator.R
@@ -1,6 +1,11 @@
 set.seed(400)
 # input: dataframe with:
-# team = 
-calculate_win_probabilities <- function(team_data) {
+# 1. team names
+# 2. name of the home team 
+# (this is the same as the previous for the home team, but not the away team)
+# 3. elo
+# 4. home field advantage
+calculate_win_probabilities <- function(team_names, home_teams, elo, hfa) {
+  win_probabilities <- rep(0, length(team_names))
   
 }
