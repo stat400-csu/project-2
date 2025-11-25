2de1416c89e081a30a8172272abcc5e9ad9159d7
Chase Bishop <cgbishop@colostate.edu>
2025-11-24 20:15:22 -0700

    csv for starting elo and function to get schedule for a week of games

--- EloStart.R
+++ EloStart.R
@@ -5,8 +5,10 @@ standings_odds <- read_csv(file)
 
 weights <- c(0.4, 0.6)
 mean_elo <- 1000
-scale <- 50
+scale <- 32
 
 start_elo <- standings_odds |>
-  mutate(Weighted_ProjW = weights[1] * W + weights[2] * ProjW) |>
-  mutate(Start_Elo = (Weighted_ProjW - 8.5) * scale + mean_elo)
+  mutate(Weighted_ProjW = round(weights[1] * W + weights[2] * ProjW, digits = 1)) |>
+  mutate(Start_Elo = round(Weighted_ProjW - 8.5) * scale + mean_elo)
+
+write_csv(start_elo, "starting_elo.csv")
