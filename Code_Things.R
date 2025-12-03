fee38f54202530b721bf0171de6725484e75da36
CarterNault <carternault23@gmail.com>
2025-12-03 14:54:09 -0700

    Update Code_Things.R

--- Code_Things.R
+++ Code_Things.R
@@ -3,4 +3,6 @@
 ELODIF <- (home_elo - away_elo)/25 #25 is a general expecect elo to point differential
 ExpectPointSpread <- (ELODIF + HFA_PointAdvantage) / Sigma_NFL #Sigma_NFL the sd is expected to be maybe 13.5 or 9 where 68%/1SD of games is has a margin between +- 13.5, 9 for this we could research this
 WinProb <- pnorm(ExpectPointSpread)
+
+#Nope
 '''
