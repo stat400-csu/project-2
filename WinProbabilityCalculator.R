a13992f479e26639969ebb54358b9b19695b577e
CarterNault <carternault23@gmail.com>
2025-12-02 23:57:55 -0700

    WinProbabilityCalculator.R
    Made a comment on line 11

--- WinProbabilityCalculator.R
+++ WinProbabilityCalculator.R
@@ -8,7 +8,7 @@ calculate_win_probabilities <- function(team_names, home_teams, elo, hfa, S) {
   for(team in 1:length(team_names)) {
     if(team_names[team] == home_teams[team]) {
       away_team_index <- get_opposite_index_of_value(home_teams, team)
-      exponent <- (-1*(elo[team] + hfa[team] - elo[away_team_index]))/S
+      exponent <- (-1*(elo[team] + hfa[team] - elo[away_team_index]))/S #I'm not sure about this line and the 2 below, look at GeneralWinProb-suggestion
       win_probabilities[team] <- 1/(1+10^exponent)
       win_probabilities[away_team_index] <- 1-win_probabilities[team]
     }
