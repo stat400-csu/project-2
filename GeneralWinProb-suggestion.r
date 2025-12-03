75adaae6e483c4695035f308e02001a7c1c8ff63
CarterNault <carternault23@gmail.com>
2025-12-03 00:18:33 -0700

    Update GeneralWinProb-suggestion.r

--- GeneralWinProb-suggestion.r
+++ GeneralWinProb-suggestion.r
@@ -1,5 +1,5 @@
 {r}
 '''
 ELODIF <- (home_elo - away_elo)/25 #25 is a general expecect elo to point differential
-ExpectPointSpread <- (ELODIF + HFA_PointAdvantage) / Sigma_NFL #Sigma_NFL the sd is expected to be 13-14 for this we could use 13.5
+ExpectPointSpread <- (ELODIF + HFA_PointAdvantage) / Sigma_NFL #Sigma_NFL the sd is expected to be 13-14 for this we could research this
 '''
