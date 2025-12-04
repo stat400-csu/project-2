66bd0e11f2a775562d97b224bb84668abe91c46e
Chase Bishop <cgbishop@colostate.edu>
2025-12-03 20:17:49 -0700

    Somewhat functioning elo updating week by week, cant handle bye weeks

--- EloStart.R
+++ EloStart.R
@@ -1,4 +1,8 @@
 library(tidyverse)
+library(nflreadr)
+source("EloUpdater.R")
+source("ScheduleLoader.R")
+source("WinProbabilityCalculator.R")
 
 standings_odds <- read_csv("2024_standings_2025_odds(in).csv")
 
@@ -13,6 +17,18 @@ team_data <- standings_odds |>
   rename(Team_Name = Tm) |>
   mutate(Weighted_ProjW = weights[1] * W + weights[2] * ProjW) |>
   mutate(Elo = (Weighted_ProjW - 8.5) * scale + mean_elo) |>
-  mutate(HFA = hfa_data$Total)
+  mutate(HFA = hfa_data$'Point Advantage') |>
+  select(TCode, Team_Name, Elo, HFA)
+  
 
-write_csv(team_data, "starting_elo.csv")
+last_week <- 13
+
+for (week in 1:last_week) {
+  current_week_schedule <- schedule_by_week(week)
+  current_week_schedule <- rename(current_week_schedule, TCode = team)
+  sched_w_elo <- left_join(team_data, current_week_schedule)
+  sched_w_elo$WP <- calculate_win_probabilities(sched_w_elo$TCode, sched_w_elo$home_team, sched_w_elo$Elo, sched_w_elo$HFA, 400)
+  sched_w_elo$Elo <- elo_updater(sched_w_elo$WP, sched_w_elo$win, sched_w_elo$Elo, scale)
+  team_data$Elo <- sched_w_elo$Elo
+}
+
